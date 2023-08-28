e2b99ad7b49a5891b439e6db6429d1c847395fb4
Himanshu <kumarhimanshu2219@gmail.com>
2023-08-28 14:40:13 +0530

    Typing master

--- 01_Typing Master/script.js
+++ 01_Typing Master/script.js
@@ -1,45 +1,71 @@
-function randomQuote() {
-  return "Hey there myself himanshu kumar tiwari and i am from daltongaj jharkhand today in this article i will show you how to make yt videos";
-}
-
 let timer = document.getElementById("timer");
 let quoteContent = document.getElementById("quoteContent");
 let inputField = document.getElementById("textArea");
 
-function init() {
-  const quote = randomQuote();
+let time = 0;
+const BASE_URL = "http://api.quotable.io/random";
+
+async function randomQuote() {
+  try {
+    const res = await fetch(BASE_URL);
+    const data = await res.json();
+    return await data.content;
+  } catch (error) {
+    (quoteContent.innerHTML = "Error : "), error;
+  }
+}
+
+async function init() {
+  const quote = await randomQuote();
   quoteContent.innerText = "";
+
   quote.split("").forEach((character) => {
     let characterSpan = document.createElement("span");
     characterSpan.innerText = character;
     quoteContent.appendChild(characterSpan);
   });
+
   startTimer();
 }
 
 inputField.addEventListener("input", () => {
   const arrayQuote = quoteContent.querySelectorAll("span");
   const arrayValue = inputField.value.split("");
+
+  let correct = true;
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index];
     if (character == null) {
       characterSpan.classList.remove("correct");
       characterSpan.classList.remove("incorrect");
+      correct = false;
     } else if (character === characterSpan.innerText) {
       characterSpan.classList.add("correct");
       characterSpan.classList.remove("incorrect");
     } else {
       characterSpan.classList.add("incorrect");
       characterSpan.classList.remove("correct");
+      correct = false;
     }
   });
+  if (correct) {
+    init();
+    setTimeout(() => {
+      inputField.value = "";
+    }, 400);
+  }
 });
 
-let time = 0;
 function startTimer() {
+  timer.innerHTML = 0;
+  time = new Date();
   setInterval(() => {
-    timer.innerText = time++;
+    timer.innerHTML = getTimerTime();
   }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - time) / 1000);
 }
 
 init();
